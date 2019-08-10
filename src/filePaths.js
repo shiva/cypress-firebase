21deddcaf15b40eaf820052b9df905d38c23c825
Shiva <shiv@shiv.me>
2019-08-10 15:23:59 -0700

    Allow override of file paths from environment variables

--- src/filePaths.js
+++ src/filePaths.js
@@ -8,20 +8,28 @@ import {
   DEFAULT_TEST_CONFIG_FILE_NAME,
 } from './constants';
 
+// override constants if provided
+const base_path = process.env.BASE_PATH || DEFAULT_BASE_PATH;
+const test_folder_path = process.env.TEST_FOLDER_PATH || DEFAULT_TEST_FOLDER_PATH;
+const firebase_config_file_name = process.env.FIREBASE_CONFIG_FILE_NAME || FIREBASE_CONFIG_FILE_NAME;
+const default_test_config_file_name = process.env.DEFAULT_TEST_CONFIG_FILE_NAME || DEFAULT_TEST_CONFIG_FILE_NAME;
+const default_test_env_file_name = process.env.DEFAULT_TEST_ENV_FILE_NAME || DEFAULT_TEST_ENV_FILE_NAME;
+const default_config_file_name = process.env.DEFAULT_CONFIG_FILE_NAME || DEFAULT_CONFIG_FILE_NAME;
+
 export const FIREBASE_CONFIG_FILE_PATH = path.join(
-  DEFAULT_BASE_PATH,
-  FIREBASE_CONFIG_FILE_NAME,
+  base_path,
+  firebase_config_file_name
 );
 export const TEST_CONFIG_FILE_PATH = path.join(
-  DEFAULT_BASE_PATH,
-  DEFAULT_TEST_CONFIG_FILE_NAME,
+  base_path,
+  default_test_config_file_name
 );
 export const TEST_ENV_FILE_PATH = path.join(
-  DEFAULT_BASE_PATH,
-  DEFAULT_TEST_ENV_FILE_NAME,
+  base_path,
+  default_test_env_file_name
 );
 export const LOCAL_CONFIG_FILE_PATH = path.join(
-  DEFAULT_BASE_PATH,
-  DEFAULT_TEST_FOLDER_PATH,
-  DEFAULT_CONFIG_FILE_NAME,
+  base_path,
+  test_folder_path,
+  default_config_file_name
 );
